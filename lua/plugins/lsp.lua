f007ff709d6a44449979267f27742428b009acf0
G30TR1X <gavriel10donovan@gmail.com>
2026-05-26 23:08:35 +0700

    Updated config to a more recent version

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,44 +6,84 @@ return {
             require("mason").setup()
         end,
     },
+
     {
         "williamboman/mason-lspconfig.nvim",
         lazy = false,
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "basedpyright", "eslint", "superhtml", "harper_ls", "kotlin_language_server" },
-            })
-        end,
         opts = {
-            auto_install = true,
+            ensure_installed = {
+                "lua_ls",
+                "clangd",
+                "basedpyright",
+                "eslint",
+                "superhtml",
+                "harper_ls",
+                "kotlin_language_server",
+            },
+            automatic_enable = true,
         },
     },
+
     {
         "neovim/nvim-lspconfig",
         lazy = false,
         config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities =
+                require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.clangd.setup({})
-            lspconfig.superhtml.setup({})
-            lspconfig.harper_ls.setup({})
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.basedpyright.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.eslint.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.kotlin_language_server.setup({
+            -- Lua
+            vim.lsp.config("lua_ls", {
                 capabilities = capabilities,
             })
 
+            -- Python
+            vim.lsp.config("basedpyright", {
+                capabilities = capabilities,
+            })
+
+            -- ESLint
+            vim.lsp.config("eslint", {
+                capabilities = capabilities,
+            })
+
+            -- Kotlin
+            vim.lsp.config("kotlin_language_server", {
+                capabilities = capabilities,
+            })
+
+            -- Clang
+            vim.lsp.config("clangd", {
+                capabilities = capabilities,
+            })
+
+            -- HTML
+            vim.lsp.config("superhtml", {
+                capabilities = capabilities,
+            })
+
+            -- Grammar / prose
+            vim.lsp.config("harper_ls", {
+                capabilities = capabilities,
+            })
+
+            -- Enable servers
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("basedpyright")
+            vim.lsp.enable("eslint")
+            vim.lsp.enable("kotlin_language_server")
+            vim.lsp.enable("clangd")
+            vim.lsp.enable("superhtml")
+            vim.lsp.enable("harper_ls")
+
+            -- Keymaps
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set(
+                { "n", "v" },
+                "<leader>ca",
+                vim.lsp.buf.code_action,
+                {}
+            )
         end,
     },
 }
